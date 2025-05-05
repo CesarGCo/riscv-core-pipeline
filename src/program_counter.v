12cb3efcbc7421212952d56a4598da7c2cc9918f
César Grandis Costa <cesargrandis38@gmail.com>
2025-05-05 15:54:30 -0300

    feat: add program counter module

---
+++ src/program_counter.v
@@ -0,0 +1,17 @@
+module program_counter (
+    input wire clk,
+    input wire reset,
+    input wire [31:0] next_pc,
+    output reg [31:0] current_pc
+);
+
+always @(posedge clk or posedge reset)begin
+    if(reset)begin
+        current_pc <= 32'b0;
+    end else begin
+        current_pc <= next_pc;
+    end
+
+end
+
+endmodule
